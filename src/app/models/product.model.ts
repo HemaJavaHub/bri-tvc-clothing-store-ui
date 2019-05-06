99bd738ef735f29d702871d44f4b3dbeed3841be
vineetbattula93 <vineet.battula@cognizant.com>
2019-05-06 16:02:43 -0400

    modified home page and product display component UI

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -9,6 +9,7 @@ public price: number;
 public category: string;
 public sub_category: string;
 public size : string;
+public image:string;
 
 
 public updateFrom(src: Product): void {
@@ -18,6 +19,7 @@ public updateFrom(src: Product): void {
     this.category = src.category;
     this.sub_category = src.sub_category;
     this.size = src.size;
+    this.image=src.image;
 
 }
 }
