7a83bad2547f4e58bd26860ef2efac749e0c0cb9
vineetbattula93 <vineet.battula@cognizant.com>
2019-05-08 11:04:47 -0400

    fixed spacing issue, added json

--- src/app/product-display-component/Productdisplay.ts
+++ src/app/product-display-component/Productdisplay.ts
@@ -5,16 +5,18 @@ export class Productdisplay {
     category: string;
     sub_category: string;
     size : string;
+    image : string;
 
 
 
-    constructor(id: number, name: string, price: number, category: string , sub_category: string, size:string) {
+    constructor(id: number, name: string, price: number, category: string , sub_category: string, size:string,image:string) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
         this.sub_category = sub_category;
         this.size = size;
+        this.image=image;
     }
 
    
